b7b79a290d78bc97dc5093e8d32883a67f5b5646
Zoe Davis <zoedavis0911@gmail.com>
2024-08-07 08:41:32 -0400

    first commit

---
+++ initials.py
@@ -0,0 +1,9 @@
+#My name is Zoe
+#I am learning Python
+print(" ZZZZZ  DDDD  ")
+print("     Z  D   D ")
+print("    Z   D   D ")
+print("   Z    D   D ")
+print("  Z     D   D ")
+print(" Z      D   D ")
+print(" ZZZZZ  DDDD  ")
